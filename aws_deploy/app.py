77182ece7cc3d5a1a330f0394541200257b98190
Ahmed Elsherbiny <ahmed.sherbiny@brighthive.io>
2025-01-09 05:01:25 +0200

    Add: logic for saving amplify app url to a dynamodb table

--- aws_deploy/app.py
+++ aws_deploy/app.py
@@ -1,5 +1,38 @@
 import boto3
+from botocore.exceptions import NoCredentialsError, PartialCredentialsError
 import os
+
+# add url to amplify table 
+def add_amplify_url(table_name, url, uuid):
+    """
+    Adds amplify default domain to amplifyUrls dynamodb table.
+
+    :param table_name: Name of the DynamoDB table.
+    :param url: amplify app default domain
+    :param uuid: workspace/<workspace-id>/project/<project-id>, for now its the amplify_app_id
+    :return: None
+    """
+    # Initialize a DynamoDB resource
+    dynamodb = boto3.resource('dynamodb')
+
+    # Reference the table
+    table = dynamodb.Table(table_name)
+    item = {
+        'workspace_project_UUID': uuid,
+        'url': url
+    }
+
+    try:
+        # Put the item into the table
+        table.put_item(Item=item)
+        print("Item added successfully.")
+
+    except NoCredentialsError:
+        print("AWS credentials not found.")
+    except PartialCredentialsError:
+        print("Incomplete AWS credentials.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 def deploy_amplify_app(app_name, repo_url, branch_name, build_image, build_directory, env_vars):
@@ -17,45 +50,22 @@ def deploy_amplify_app(app_name, repo_url, branch_name, build_image, build_direc
             accessToken=gh_access_token
         )
         app_id = app_response['app']['appId']
+        app_url = app_response['app']['defaultDomain']
         print(f"Amplify app created with App ID: {app_id}")
+      
+        # add it to dynamodb
+        full_url = 'https://' + branch_name + app_url
+        # we should use the uuid instaed of the app_id here
+        add_amplify_url('amplifyUrls', full_url, app_id)
 
         # Step 2: Create Branch and Set BuildSpec
         print(f"Creating branch '{branch_name}'...")
         branch_response = amplify_client.create_branch(
             appId=app_id,
             branchName=branch_name,
-            environmentVariables=env_vars,
+            environmentVariables=env_vars
         )
         print(f"Branch '{branch_name}' created.")
-
-        # Step 3: Update BuildSpec for the Branch
-        build_spec = f"""
-version: 1.0
-frontend:
-  phases:
-    preBuild:
-      commands:
-        - npm ci --cache .npm --prefer-offline
-    build:
-      commands:
-        - npm run sources && npm run build 
-  artifacts:
-    baseDirectory: {build_directory}
-    files:
-      - '**/*'
-  cache:
-    paths:
-      - .npm/**/*
-
-image: {build_image}
-        """
-        print("Updating build spec...")
-        amplify_client.update_branch(
-            appId=app_id,
-            branchName=branch_name,
-            buildSpec=build_spec
-        )
-        print("Build spec updated successfully.")
 
         amplify_client.start_job(
             appId=app_id,
@@ -75,7 +85,7 @@ if __name__ == "__main__":
     redshift_username = os.getenv('redshift_username')
     redshift_password = os.getenv('redshift_password')
     REPO_URL = os.getenv('repo_url', 'https://github.com/brighthive-workspaces/evidence-dev-test')
-    BRANCH_NAME = os.getenv('branch_name', 'main')
+    BRANCH_NAME = os.getenv('branch_name', 'deploy-script')
 
     # Custom Build Image and Build Directory
     BUILD_IMAGE = 'public.ecr.aws/docker/library/node:20-bookworm'
@@ -84,9 +94,10 @@ if __name__ == "__main__":
     # Environment Variables
     ENV_VARS = {
         'EVIDENCE_SOURCE__brighthive_dev_redshift__user': redshift_username,
-        'EVIDENCE_SOURCE__brighthive_dev_redshift__password': redshift_password
+        'EVIDENCE_SOURCE__brighthive_dev_redshift__password': redshift_password,
+        '_CUSTOM_IMAGE': 'public.ecr.aws/docker/library/node:20-bookworm'
     }
 
-    app_name = 'custom_app2'
+    APP_NMAE = 'evidence_deploy_script_1'
 
-    deploy_amplify_app(app_name, REPO_URL, BRANCH_NAME, BUILD_IMAGE, BUILD_DIRECTORY, ENV_VARS)
+    deploy_amplify_app(APP_NMAE, REPO_URL, BRANCH_NAME, BUILD_IMAGE, BUILD_DIRECTORY, ENV_VARS)
